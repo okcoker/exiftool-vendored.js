9e5dcd44873d1833e4420883801d6de40cea3cd5
Sean Coker <1030830+okcoker@users.noreply.github.com>
2022-01-26 00:30:22 -0500

    Use node buffer import

--- src/deps.ts
+++ src/deps.ts
@@ -4,16 +4,17 @@ export * as ProgressBar from "https://cdn.skypack.dev/progress@2.0.3";
 export * as luxon from "https://cdn.skypack.dev/luxon@2.3.0";
 export * as BatchCluster from "https://cdn.skypack.dev/batch-cluster@9.0.1";
 export * as he from "https://cdn.skypack.dev/he@1.2.0";
+export * as Buffer from "https://deno.land/std@0.122.0/node/buffer.ts"
 // https://stackoverflow.com/questions/61813646/whats-deno-equivalent-of-node-js-buffer-fromstring
-export class Buffer {
-    static from(str: string, encoding = 'hex') {
-        if (encoding === 'base64') {
-            return btoa(str);
-        }
+// export class Buffer {
+//     static from(str: string, encoding = 'hex') {
+//         if (encoding === 'base64') {
+//             return btoa(str);
+//         }
 
-        const encoder = new TextEncoder()
-        return encoder.encode(str);
-    }
-}
+//         const encoder = new TextEncoder()
+//         return encoder.encode(str);
+//     }
+// }
 
 export const getOSTempDir = () => Deno.env.get('TMPDIR') || Deno.env.get('TMP') || Deno.env.get('TEMP') || '/tmp';
